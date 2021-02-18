5e540bdb19de03d54c46e6483b3aef7c7acdecf1
little-sunj <jsw5666@gmail.com>
2021-02-19 08:20:46 +0900

    test코드 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+    @Test
+    public void 중복_회원_예약() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+}
